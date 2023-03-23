9f2913058a3fc50e7c8624f3a7ddefb77bbd30e3
Daniel Galdino <danoligal@gmail.com>
2023-03-22 22:06:53 -0300

    Variaveis do js

---
+++ index.js
@@ -0,0 +1,11 @@
+//Variáveis numéricas//
+var $number = 10;
+var $number2 = 5.5;
+
+console.log($number, $number2);
+
+//Variável de cadeia de caracteres (string)//
+var $string = 'Olá mundo';
+$string = 'Hello word';
+
+console.log($string);
